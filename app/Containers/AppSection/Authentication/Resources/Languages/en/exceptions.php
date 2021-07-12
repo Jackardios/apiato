9e5b204af623c23236fba83cb794bf6305ff0096
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-07-12 13:10:22 +0300

    add exception messages languages

---
+++ app/Containers/AppSection/Authentication/Resources/Languages/en/exceptions.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Exception Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language strings are used for exception messages
+    |
+    */
+
+    'login-failed' => 'An Exception happened during the Login Process.',
+    'oauth' => 'OAuth 2.0 is not installed.',
+    'refresh-token-missed' => 'We could not find the Refresh Token. Maybe none is provided?',
+    'user-not-confirmed' => 'The user email is not confirmed yet. Please verify your user before trying to login.'
+];
